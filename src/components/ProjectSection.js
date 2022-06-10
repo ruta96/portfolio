700a3e46566c2a178c60730ae951e98d92b75567
Michał Ruta <ruciak1996@gmail.com>
2022-06-10 21:12:49 +0200

    link update

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -13,10 +13,10 @@ function ProjectSection() {
             <div className='project-braces project-braces-open'>&#123;</div>
             <div className='project-gallery'>
                 <Project js css scss html name="sabinakluz.pl" link="https://sabinakluz.pl" />
-                <Project node js es6 name="sweetroll bot" link="#" />
+                <Project node js es6 name="sweetroll bot" link="https://github.com/ruta96/discord" />
                 <Project js css html bootstrap name="okw-rutex.pl" link="https://rutex.netlify.app/" />
                 <Project js css es6 html scss name="watched movies" link="https://movieslistproject.netlify.app/" />
-                <Project js css es6 html scss react name="michalruta.pl" link="/" />
+                <Project js css es6 html scss react name="michalruta.pl" link="https://github.com/ruta96/portfolio" />
             </div>
             <div className='project-braces project-braces-close'>&#125;</div>
         </div>
